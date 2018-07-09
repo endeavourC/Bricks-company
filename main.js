172e7ece000437e2fcc2cfe3cb5a2f33f3040944
endeavourC <35568078+endeavourC@users.noreply.github.com>
2018-07-09 21:42:57 +0200

    change page

--- main.js
+++ main.js
@@ -1,122 +1,44 @@
-const cityNameInp = document.querySelector('#city-to-check');
-let cityTitle = document.querySelector('.city-title');
-let cityCountry = document.querySelector('.city-country');
-let cityTemp = document.querySelector('.city-temp');
-let cityWind = document.querySelector('.city-wind');
-let cityPressure = document.querySelector('.city-pressure');
-let cityDataOfDay = document.querySelector('.city-data-of-day');
-let cityImage = document.querySelector('.city-image');
-let cityExists = false;
-let name;
-let dataLoaded = false;
-cityNameInp.addEventListener('keyup', checkWeather);
+function toggleMenu() {
+    $('#nav-icon').toggleClass('open');
+    $('nav').toggleClass('toggle');
+}
 
-function loadImage(url) {
-    return fetch(url)
-        .then(data => data.json())
-        .then(data => {
-            if (data.status === "OK") {
-                let reference = data.results[0].photos[0].photo_reference;
-                if (reference) {
-                    cityTitle.innerHTML = `${data.results[0].name},`;
-                    name = data.results[0].name;
-                    cityImage.style.backgroundImage = `url(https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&photoreference=${reference}&key=AIzaSyAVEoBlH2p65Whg0bJsP3TeJfvTMXBqMFc)`
-                    cityExists = true;
-                }
-            } else {
-                cityExists = false;
-                if (cityExists === false) {
-                    cityTitle.innerHTML = ``;
-                    cityCountry.innerHTML = `Przepraszamy, nie znaleźliśmy  miasta ${name}.`;
-                    cityTemp.innerHTML = ``;
-                    cityWind.innerHTML = ``;
-                    cityPressure.innerHTML = ``;
-                    cityDataOfDay.innerHTML = ``;
-                }
-                return false;
-            }
+function togglePrimaryMenu() {
+    $('.primary-list').toggleClass('toggle');
+    if ($('.primary-list').hasClass('toggle')) {
+        $('.primary-list').css({
+            overflow: 'visible',
         })
-        .catch(err => console.log(err))
-}
-
-function convertDegrees(number) {
-    return Math.floor(number - 273) + "℃";
-}
-
-function convertWindUnit(number) {
-    return Math.floor(number * 3600 / 1000) + "km/h";
-}
-
-function leadingZero(i) {
-    return (i < 10) ? '0' + i : i;
-}
-
-function getDate(callback) {
-    const date = new Date();
-    return `${callback(date.getDate())}.${callback(date.getMonth())}.${callback(date.getFullYear())} ${callback(date.getHours())}:${callback(date.getMinutes())}:${date.getSeconds()}`
-}
-
-function showArticle() {
-    document.querySelector('.typed-city-container').classList.add('fadein')
-}
-
-function loadWeatherInformation(url) {
-    return fetch(url)
-        .then(data => data.json())
-        .then(data => {
-            if (data.cod === 200) {
-                dataLoaded = true;
-                if (dataLoaded) {
-                    cityCountry.innerHTML = `kraj: ${data.sys.country}`;
-                    cityTemp.innerHTML = `temperatura: ${convertDegrees(data.main.temp)}`;
-                    cityWind.innerHTML = `wiatr: ${convertWindUnit(data.wind.speed)} `;
-                    cityPressure.innerHTML = `ciśnienie:  ${data.main.pressure} hPa`;
-                    cityDataOfDay.innerHTML = `dane na dzień: ${getDate(leadingZero)}`
-                }
-
-            } else {
-                dataLoaded = false;
-                cityCountry.innerHTML = `Przepraszamy, znaleźlismy miasto ale nie mamy danych dotyczących temperatury.`;
-                cityTemp.innerHTML = ``;
-                cityWind.innerHTML = ``;
-                cityPressure.innerHTML = ``;
-                cityDataOfDay.innerHTML = ``;
-            }
-
+    } else {
+        $('.primary-list').css({
+            overflow: 'hidden',
         })
-        .catch(err => console.log(err))
-}
-
-function checkWeather(e) {
-    e.preventDefault();
-    if (e.keyCode === 13) {
-        this.blur();
-        document.querySelector('.preloader').style.opacity = 1;
-        document.querySelector('.preloader').style.zIndex = 1;
-        const reg = /^\D+$/;
-        name = this.value;
-        this.value = '';
-        if (name != '') {
-            if (reg.test(name)) {
-                if (name != undefined) {
-                    showArticle();
-                    loadImage(`https://cors.io/?https://maps.googleapis.com/maps/api/place/textsearch/json?query=${name}&key=AIzaSyAVEoBlH2p65Whg0bJsP3TeJfvTMXBqMFc`)
-                        .then(function () {
-                            if (cityExists === true) {
-                                loadWeatherInformation(`https://cors.io/?http://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=bc4acb56e5405a03961d16e5733e0d79`)
-                            } else {
-                                return false;
-                            }
-                        })
-                        .then(() => {
-                            setTimeout(() => {
-                                document.querySelector('.preloader').style.opacity = 0;
-                                document.querySelector('.preloader').style.zIndex = -2;
-                            }, 500)
-                        });
-                }
-
-            }
-        }
     }
 }
+$('#nav-icon-container').on('click', toggleMenu);
+$('.products-list').on('click', togglePrimaryMenu);
+
+
+
+function clearfx(element, name) {
+    return $(element).removeClass(name);
+}
+$('.bramy-product').on('click', function () {
+    $(this).toggleClass('show').siblings('div').removeClass('show');
+    $('.bramy-product h3,.bramy-product>ul>li,.bramy-product > p').css({
+        opacity: 0,
+    })
+    if ($(this).hasClass('show')) {
+        clearfx('.bramy-product', 'show')
+        $(this).addClass('show');
+        $(this).find('p,li,h3').css({
+            opacity: 1,
+        })
+    } else {
+        $(this).find('p,li,h3').css({
+            opacity: 0,
+        })
+
+    }
+
+})
