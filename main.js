6aad0fa31941dc446fe86b7b4b2efc986c3a70bb
endeavourC <35568078+endeavourC@users.noreply.github.com>
2018-09-04 21:07:04 +0200

    s
    s

--- main.js
+++ main.js
@@ -1,147 +1,183 @@
-$('.services-list li, .services h3,.about-text,.about img,.offer h3, .offer article img, .offer-text,.creating-process,#contact-form input,.contact article h3').addClass('hidden');
-$(window).on('load', function () {
+window.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.preloader').classList.add('loaded');
 
-    $('.page-loader').fadeOut(700);
-    const pageSlider = {
-        slideLength: $('.slider-container').children().length,
-        slider: $('.slider-container'),
-        easingTime: 700,
-        easing: 'easeInOutExpo',
-        rightBtn: $('.fa-arrow-right'),
-        leftBtn: $('.fa-arrow-left'),
-        interval: null,
-        intervalTime: 3000,
-        slidein: function () {
-            let elements = $('.slider-container').find('.slideshow');
-            if (!this.slider.is(":animated")) {
-                this.slider.animate({
-                    marginLeft: '-100vw',
-                }, this.easingTime, this.easing, function () {
-                    elements.last().after(elements.first());
-                    this.slider.css({
-                        marginLeft: 0,
-                    })
-                }.bind(this))
-            }
-        },
-        switchRight: function () {
-            this.rightBtn.on('click', function (elements) {
-                clearInterval(this.interval);
-                elements = $('.slider-container').find('.slideshow');
-                if (!this.slider.is(":animated")) {
-                    this.slider.animate({
-                        marginLeft: '-100vw',
-                    }, this.easingTime, this.easing, function () {
-                        elements.last().after(elements.first());
-                        this.slider.css({
-                            marginLeft: 0,
-                        })
-                    }.bind(this))
-                    this.interval = setInterval(function () {
-                        this.slidein();
-                    }.bind(this), this.intervalTime);
-                }
-            }.bind(this))
-        },
-        switchLeft: function () {
-            this.leftBtn.on('click', function (elements) {
-                clearInterval(this.interval);
-                elements = $('.slider-container').find('.slideshow');
-                if (!this.slider.is(":animated")) {
-                    this.slider.css({
-                        marginLeft: '-100vw',
-                    })
-                    elements.first().before(elements.last());
-                    this.slider.animate({
-                        marginLeft: 0,
-                    }, this.easingTime, this.easing);
-                    this.interval = setInterval(function () {
-                        this.slidein();
-                    }.bind(this), this.intervalTime)
-                }
-            }.bind(this))
-        },
-        doInterval: function () {
-            $(window).on('focus', function () {
-                this.interval = setInterval(function () {
-                    this.slidein();
-                }.bind(this), this.intervalTime)
-            }.bind(this));
-            $(window).on('blur', function () {
-                clearInterval(this.interval);
-            }.bind(this))
-            this.interval = setInterval(function () {
-                this.slidein();
-            }.bind(this), this.intervalTime)
-        },
-        start: function () {
-            this.switchRight();
-            this.switchLeft();
-            this.doInterval();
-        }
+    let takenQuest = [];
+    let currentQuest = [];
 
+    let img = document.querySelector('.category-image');
+    let question = document.querySelector('.question');
+
+    function sortArray(array) {
+        return array.sort(function () {
+            return Math.random() - 0.5;
+        });
     }
-    pageSlider.start();
 
-    function resizeHeader() {
-        if (pageYOffset > 0) {
-            $('.page-header').height(60)
-        } else {
-            $('.page-header').height(80)
-        }
-    };
+    function showAnswer(currentAnswer) {
+        document.querySelector('.first-answer').innerHTML = currentAnswer[0];
+        document.querySelector('.second-answer').innerHTML = currentAnswer[1];
+        document.querySelector('.third-answer').innerHTML = currentAnswer[2];
+        document.querySelector('.four-answer').innerHTML = currentAnswer[3];
+    }
+    let currentAnswer;
+    let prices = [100, 500, 1000, 2000, 4000, 8000, 16000, 32000, 125000, 250000, 500000, 1000000];
+    let value = 0;
+    let winPrize;
 
-    function showElements(elements, mode, del) {
-        let offset = pageYOffset;
-        let height = $(window).height();
-        let position = elements.offset().top;
-        let method = mode;
-        let distance = 300;
-        let elToShow = elements;
-        if (method != undefined) {
-            if (position != undefined) {
-                if (offset - distance > position - height) {
-                    elements.removeClass(del);
-                    elements.addClass(method);
+    function showQuestion() {
+        let rand = Math.floor(Math.random() * questions.length);
+        if (takenQuest.length < questions.length) {
+            for (i = 0; i < takenQuest.length; i++) {
+                if (takenQuest[i] === questions[rand]) {
+                    return showQuestion();
                 }
             }
+            console.log(currentQuest)
+            console.log(takenQuest);
+            takenQuest.push(questions[rand]);
+            currentQuest.push(questions[rand]);
+            question.innerHTML = currentQuest[0].name;
+            img.setAttribute('src', `images/${currentQuest[0].category}.jpg`);
+            currentAnswer = [currentQuest[0].correctAnswer, currentQuest[0].answers[0], currentQuest[0].answers[1], currentQuest[0].answers[2]];
+            winPrize = prices[value];
+            document.querySelector('.prize').innerHTML = "Twoja wygrana: " + winPrize;
+            sortArray(currentAnswer);
+            showAnswer(currentAnswer);
         }
-    }
 
-    function moveTo(e, ahref) {
-        e.preventDefault();
-        $('#nav-icon').removeClass('open');
-        $('.page-navigation').removeClass('active');
-        ahref = $(this).attr('href');
-        if (!$('body,html').is(':animated')) {
-
-            $('body,html').animate({
-                scrollTop: $(ahref).offset().top - 60 + 'px',
-            }, 1000, 'easeInOutExpo')
-        }
-    }
-    $('.page-navigation ul li a').on('click', moveTo);
-
-    function toggleMenu() {
-        $('#nav-icon').toggleClass('open');
-        $('.page-navigation').toggleClass('active');
     };
-    $('#nav-icon-container').on('click', toggleMenu)
-    $(window).on('scroll', function () {
-        resizeHeader();
-        showElements($('.services-list li'), 'fadeIn', 'hidden');
-        showElements($('.services h3'), 'fadeInLeft', 'hidden');
-        showElements($('.about-text'), 'fadeInLeft', 'hidden');
-        showElements($('.about img'), 'fadeInLeft', 'hidden');
-        showElements($('.offer h3'), 'fadeInTop', 'hidden');
-        showElements($('.offer article img'), 'fadeInLeft', 'hidden');
-        showElements($('.offer-text'), 'fadeInTop', 'hidden');
-        showElements($('.creating-process'), 'fadeInLeft', 'hidden');
-        showElements($('#contact-form input'), 'fadeInLeft', 'hidden');
-        showElements($('.contact article h3'), 'fadeInTop', 'hidden');
-
-
-
+    showQuestion();
+    document.querySelectorAll('.answer').forEach(el => {
+        el.addEventListener('click', checkAnswer)
     })
+    document.querySelector('.half').addEventListener('click', removeHalf);
+    let halfClicked = false;
+
+    function removeHalf() {
+        if (!halfClicked) {
+            let x = Math.floor(Math.random() * 4);
+            let y = Math.floor(Math.random() * 4);
+            if (currentQuest[0].correctAnswer === currentAnswer[x] || currentQuest[0].correctAnswer === currentAnswer[y]) {
+                return removeHalf();
+            } else {
+                if (currentAnswer[x] != currentAnswer[y]) {
+                    currentAnswer[x] = '';
+                    currentAnswer[y] = '';
+                    halfClicked = true;
+                    showAnswer(currentAnswer)
+                    document.querySelector('.half').classList.add('clicked');
+                } else {
+                    return removeHalf();
+                };
+            }
+        }
+        console.log(currentAnswer)
+    }
+    document.querySelector('.ok').addEventListener('click', function () {
+        this.parentElement.classList.remove('on');
+    })
+    document.querySelector('.friend').addEventListener('click', askFriend);
+    let friendAsked = false;
+    document.querySelector('.change').addEventListener('click', changeQuestion);
+    let questionChanged = false;
+
+    function changeQuestion() {
+        if (!questionChanged) {
+            currentQuest.length = 0;
+            showQuestion();
+            questionChanged = true;
+            this.classList.add('clicked');
+        }
+    };
+
+    function askFriend() {
+        if (!friendAsked) {
+            this.classList.add('clicked');
+            let modal = document.querySelector('.friend-modal');
+            let modalAnswer = document.querySelector('.friend-modal-answer');
+            modalAnswer.innerHTML = currentQuest[0].correctAnswer;
+            friendAsked = true;
+            modal.classList.add('on');
+        }
+
+    }
+    let priceDOM = document.querySelectorAll('.price');
+
+
+    function delayThat(callback, time) {
+        return setTimeout(function () {
+            callback();
+        }, time);
+    }
+
+    function doWhenWin() {
+        if (value === 10) {
+            document.querySelector('.lost-modal').classList.add('on');
+            document.querySelector('.lost-modal p').innerHTML = "Wygrałeś 1 000 000 zł!"
+        } else {
+            currentQuest.length = 0;
+            value++;
+            priceDOM[value].classList.add('have');
+            showQuestion();
+            document.querySelectorAll('.answer').forEach(el => {
+                el.classList.remove('good')
+            })
+        }
+    }
+
+    function showGreen() {
+        document.querySelectorAll('.answer').forEach(el => {
+            el.classList.remove('checking')
+        })
+        this.classList.add('good');
+    }
+
+    function showRed() {
+        document.querySelectorAll('.answer').forEach(el => {
+            el.classList.remove('checking')
+        })
+        this.classList.add('wrong');
+    }
+
+    function doWhenLose() {
+        document.querySelector('.lost-modal p').innerHTML = "Przegrałeś twoja wygrana to: " + winPrize;
+        document.querySelector('.lost-modal').classList.add('on')
+    };
+
+    function replayGame() {
+        currentQuest.length = 0;
+        winPrize;
+        value = 0;
+        takenQuest.length = 0;
+        friendAsked = false;
+        questionChanged = false;
+        document.querySelectorAll('.answer').forEach(el => {
+            el.classList.remove('checking')
+            el.classList.remove('wrong');
+            el.classList.remove('good');
+        })
+        priceDOM.forEach(el => {
+            el.classList.remove('have');
+        })
+        priceDOM[0].classList.add('have');
+        this.parentElement.classList.remove('on');
+        showQuestion();
+
+    }
+    document.querySelector('.replay').addEventListener('click', replayGame);
+
+    function checkAnswer() {
+
+        this.classList.add('checking');
+        if (this.innerHTML === currentQuest[0].correctAnswer) {
+
+            delayThat(showGreen.bind(this), 1000)
+            delayThat(doWhenWin, 1500);
+
+        } else {
+            delayThat(showRed.bind(this), 1000);
+            delayThat(doWhenLose, 1100);
+        }
+    }
 
 });
